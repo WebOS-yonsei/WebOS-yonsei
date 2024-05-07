fef04f887c6f3c943e45e45b3aeb713b03f8b178
gitchan <eunkeeeee@gmail.com>
2024-05-07 12:27:35 +0900

    feat: add exception handler

---
+++ server/src/main/java/server/server/api/GlobalExceptionHandler.java
@@ -0,0 +1,23 @@
+package server.server.api;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.coyote.BadRequestException;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+
+import static org.springframework.http.HttpStatus.BAD_REQUEST;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+    @ExceptionHandler(BadRequestException.class)
+    public ResponseEntity<String> handleBadRequestException(final BadRequestException exception) {
+        log.warn("BadRequestException | " + exception.getMessage());
+        return ResponseEntity
+                .status(BAD_REQUEST.value())
+                .body(exception.getMessage());
+    }
+}
