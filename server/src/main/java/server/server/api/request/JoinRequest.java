208ed95bc2ec7ad5b98ccadcfd6858ce5886f31c
gitchan <eunkeeeee@gmail.com>
2024-05-13 12:15:23 +0900

    feat: 회원가입 기능

---
+++ server/src/main/java/server/server/api/request/JoinRequest.java
@@ -0,0 +1,13 @@
+package server.server.api.request;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class JoinRequest {
+
+    private final String loginId;
+
+    private final String password;
+}
--- server/src/main/java/server/server/entity/Users.java
+++ server/src/main/java/server/server/entity/Users.java
@@ -5,8 +5,10 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import server.server.util.PasswordUtil;
 
 import static lombok.AccessLevel.PROTECTED;
 
@@ -23,4 +25,20 @@ public class Users {
     private String username;
     private String loginId;
     private String password;
+
+    @Builder(builderMethodName = "basicLogin")
+    public Users(
+            final String loginId,
+            final String password
+    ) {
+        this.loginId = loginId;
+        this.password = encryptPassword(password);
+    }
+
+    private static String encryptPassword(final String password) {
+        if (password != null) {
+            return PasswordUtil.encrypt(password);
+        }
+        return null;
+    }
 }
