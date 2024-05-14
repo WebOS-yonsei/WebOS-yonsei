bae97a55e9f1a35d35515a28b36028e257ca55ce
jinoov <jinho020@yonsei.ac.kr>
2024-05-14 10:58:25 +0900

    [client] feat: video playing page ui

--- client/src/features/video/video-playing-page.tsx
+++ client/src/features/video/video-playing-page.tsx
@@ -1,12 +1,16 @@
 import { Heading, VStack } from '@chakra-ui/react';
 import { VideoLayout } from './video-layout';
+import { VideoPlayer } from './video-player';
 
 export function VideoPlayingPage({ videoId }: { videoId: string }) {
+  const MOCK_URL = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
+
   return (
     <VideoLayout>
       <VStack align="stretch" spacing={20}>
         <VStack align="stretch" spacing={5}>
           <Heading fontSize="2xl">{videoId}</Heading>
+          <VideoPlayer src={MOCK_URL} />
         </VStack>
       </VStack>
     </VideoLayout>
