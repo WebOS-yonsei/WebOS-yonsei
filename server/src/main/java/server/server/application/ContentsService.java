67f9e0d77ad718ff64f2437eeb33ccc54fdd1bc2
HyunjungKim323 <119280993+HyunjungKim323@users.noreply.github.com>
2024-05-14 21:47:47 +0900

    [server] feat: 영상 리스트 조회

--- server/src/main/java/server/server/application/ContentsService.java
+++ server/src/main/java/server/server/application/ContentsService.java
@@ -2,11 +2,17 @@ package server.server.application;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import server.server.entity.Contents;
 import server.server.repository.ContentsRepository;
+
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
 public class ContentsService {
     private final ContentsRepository contentsRepository;
 
+    public static List<Contents> findAllVideos() {
+        return null;
+    }
 }
