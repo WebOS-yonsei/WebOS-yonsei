137e83b5d0aff5d7772ee3b10874fd2b2386d81b
jinoov <jinho020@yonsei.ac.kr>
2024-05-18 09:25:38 +0900

    [client] chore: terser and case-sensitive plugin

--- client/config/webpack.config.js
+++ client/config/webpack.config.js
@@ -2,7 +2,6 @@
 
 const fs = require('fs');
 const path = require('path');
-const CaseSensitivePathsPlugin = require('case-sensitive-paths-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
 const ForkTsCheckerWebpackPlugin = process.env.TSC_COMPILE_ON_ERROR === 'true' ? require('react-dev-utils/ForkTsCheckerWarningWebpackPlugin') : require('react-dev-utils/ForkTsCheckerWebpackPlugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
@@ -14,7 +13,7 @@ const NodePolyfillPlugin = require('node-polyfill-webpack-plugin');
 const resolve = require('resolve');
 const TerserPlugin = require('terser-webpack-plugin');
 const { DefinePlugin, EnvironmentPlugin } = require('webpack');
-const { optionParser: app, cssModuleIdent: getLocalIdent, GracefulFsPlugin, ILibPlugin, WebOSMetaPlugin } = require('@enact/dev-utils');
+const { optionParser: app, cssModuleIdent: getLocalIdent, GracefulFsPlugin, WebOSMetaPlugin } = require('@enact/dev-utils');
 const createEnvironmentHash = require('./createEnvironmentHash');
 const SpeedMeasurePlugin = require('speed-measure-webpack-plugin');
 
@@ -25,23 +24,6 @@ module.exports = function (env, contentHash = false, isomorphic = false, noAnima
 
   // Load applicable .env files into environment variables.
   require('./dotenv').load(app.context);
-
-  // Sets the browserslist default fallback set of browsers to the Enact default browser support list.
-  app.setEnactTargetsAsDefault();
-
-  // Check if JSX transform is able
-  const hasJsxRuntime = (() => {
-    if (process.env.DISABLE_NEW_JSX_TRANSFORM === 'true') {
-      return false;
-    }
-
-    try {
-      require.resolve('react/jsx-runtime');
-      return true;
-    } catch (e) {
-      return false;
-    }
-  })();
 
   // Check if TypeScript is setup
   const useTypeScript = fs.existsSync('tsconfig.json');
@@ -247,43 +229,9 @@ module.exports = function (env, contentHash = false, isomorphic = false, noAnima
       // These are only used in production mode
       minimizer: [
         new TerserPlugin({
-          terserOptions: {
-            parse: {
-              // we want uglify-js to parse ecma 8 code. However, we don't want it
-              // to apply any minfication steps that turns valid ecma 5 code
-              // into invalid ecma 5 code. This is why the 'compress' and 'output'
-              // sections only apply transformations that are ecma 5 safe
-              // https://github.com/facebook/create-react-app/pull/4234
-              ecma: 8,
-            },
-            compress: {
-              ecma: 5,
-              warnings: false,
-              // Disabled because of an issue with Uglify breaking seemingly valid code:
-              // https://github.com/facebook/create-react-app/issues/2376
-              // Pending further investigation:
-              // https://github.com/mishoo/UglifyJS2/issues/2011
-              comparisons: false,
-              // Disabled because of an issue with Terser breaking valid code:
-              // https://github.com/facebook/create-react-app/issues/5250
-              // Pending futher investigation:
-              // https://github.com/terser-js/terser/issues/120
-              inline: 2,
-            },
-            mangle: {
-              safari10: true,
-            },
-            output: {
-              ecma: 5,
-              comments: false,
-              // Turned on because emoji and regex is not minified properly using default
-              // https://github.com/facebook/create-react-app/issues/2488
-              ascii_only: true,
-            },
-          },
           // Use multi-process parallel running to improve the build speed
-          // Default number of concurrent runs: os.cpus().length - 1
-          parallel: true,
+          minify: TerserPlugin.esbuildMinify,
+          terserOptions: {},
         }),
         new CssMinimizerPlugin(),
       ],
@@ -337,8 +285,6 @@ module.exports = function (env, contentHash = false, isomorphic = false, noAnima
       new NodePolyfillPlugin(),
       // Provide meaningful information when modules are not found
       new ModuleNotFoundPlugin(app.context),
-      // Ensure correct casing in module filepathes
-      new CaseSensitivePathsPlugin(),
       // Switch the internal NodeOutputFilesystem to use graceful-fs to avoid
       // EMFILE errors when hanndling mass amounts of files at once, such as
       // what happens when using ilib bundles/resources.
