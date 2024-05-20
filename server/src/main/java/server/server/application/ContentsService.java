952aa9155acfc26411bff4435f441f9e1ba0cca0
gitchan <eunkeeeee@gmail.com>
2024-05-20 15:20:50 +0900

    feat: apply login to contents

--- server/src/main/java/server/server/application/ContentsService.java
+++ server/src/main/java/server/server/application/ContentsService.java
@@ -6,9 +6,9 @@ import server.server.entity.Contents;
 import server.server.exception.ContentNotFoundException;
 import server.server.repository.ContentsRepository;
 
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
-import java.util.Collections;
 
 @Service
 @RequiredArgsConstructor
@@ -16,10 +16,9 @@ public class ContentsService {
     private final ContentsRepository contentsRepository;
 
     // contents list 조회
-    public List<Contents> getContents(String sessionId) {
+    public List<Contents> getContents(final Long userId) {
 
         // userId 조회 @Session
-        // Long userId = getUserId(sessionId);
         long id = 1L;
 
         Optional<Contents> optionalContents = contentsRepository.findById(id);
