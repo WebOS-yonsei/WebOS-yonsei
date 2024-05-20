7c379170e17ec9ca6bfeee0c528a5de45ef77eb0
gitchan <eunkeeeee@gmail.com>
2024-05-20 14:57:57 +0900

    test: acceptance test for join, login

---
+++ server/src/test/java/server/server/acceptance/step/UsersStep.java
@@ -0,0 +1,25 @@
+package server.server.acceptance.step;
+
+import io.restassured.RestAssured;
+import io.restassured.response.ExtractableResponse;
+import io.restassured.response.Response;
+import server.server.api.request.JoinRequest;
+
+import static io.restassured.http.ContentType.JSON;
+
+public class UsersStep {
+
+    public static ExtractableResponse<Response> 회원가입_요청(JoinRequest request) {
+        return RestAssured.given()
+                .log().all()
+                .contentType(JSON)
+                .body(request)
+
+                .when()
+                .post("/users/join")
+
+                .then()
+                .log().all()
+                .extract();
+    }
+}
