a4ae355fd17cc21003378e5ad00d4de4b9b5993d
2020147542 <tmfal5023@yonsei.ac.kr>
2024-05-20 20:49:53 +0900

    [server] refactor: 프로필 리스트 조회 테스트 코드 수정 완료

--- server/src/test/java/server/server/acceptance/ProfileAcceptanceTest.java
+++ server/src/test/java/server/server/acceptance/ProfileAcceptanceTest.java
@@ -3,35 +3,34 @@ package server.server.acceptance;
 import io.restassured.RestAssured;
 import io.restassured.response.ExtractableResponse;
 import io.restassured.response.Response;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.HttpStatus;
 import server.server.api.request.JoinRequest;
 import server.server.api.request.LoginRequest;
+import server.server.api.request.ProfileRequest;
 import server.server.common.AcceptanceTest;
 import server.server.entity.Contents;
 import server.server.entity.Grade;
-import server.server.entity.Profile;
 import server.server.entity.ProfileContents;
 import server.server.repository.ContentsRepository;
 import server.server.repository.ProfileContentsRepository;
 import server.server.repository.ProfileRepository;
 
-import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
 import static io.restassured.http.ContentType.JSON;
 import static org.assertj.core.api.Assertions.assertThat;
-import static org.mockito.ArgumentMatchers.any;
+import static server.server.acceptance.step.ProfilesStep.프로필_생성_요청;
 import static server.server.acceptance.step.UsersStep.로그인_요청하고_세션_아이디_반환;
 import static server.server.acceptance.step.UsersStep.회원가입_요청;
 
 class ProfileAcceptanceTest extends AcceptanceTest {
 
-    @MockBean
+    @Autowired
     private ProfileRepository profileRepository;
 
     @MockBean
@@ -41,59 +40,57 @@ class ProfileAcceptanceTest extends AcceptanceTest {
     private ContentsRepository contentsRepository;
 
     @Test
-    void server_profile_create() {
+    void 프로필_생성을_할_수_있다() {
 
         // given
-        File jsonFile = new File("src/test/resources/mock/profileRequest.json");
-        Profile profile = Profile.builder()
-                .id(2L)
-                .userId(1L)
-                .nickname("프로필1")
+        final ProfileRequest profileRequest = ProfileRequest.builder()
+                .nickname("gitchan")
+                .profileUri("https://3rkewj4g32rk41r5t45r")
+                .grade(Grade.ADULT)
+                .profilePassword("0123")
                 .build();
-        Mockito.when(profileRepository.save(any(Profile.class))).thenReturn(profile);
 
         회원가입_요청(JoinRequest.of("gitchan", "webos"));
         final String sessionId = 로그인_요청하고_세션_아이디_반환(LoginRequest.of("gitchan", "webos"));
 
         // when
-        final ExtractableResponse<Response> response = RestAssured.given()
-                .log().all()
-                .contentType(JSON)
-                .header("Authorization", sessionId)
-                .body(jsonFile)
-
-                .when()
-                .post("/profiles")
-
-                .then()
-                .log().all()
-                .extract();
+        final ExtractableResponse<Response> response = 프로필_생성_요청(sessionId, profileRequest);
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.CREATED.value());
+        assertThat(response.header("Location")).isNotBlank();
     }
 
-    @Disabled
     @Test
-    void server_profileList() {
+    void 프로필_리스트_조회를_할_수_있다() {
         // given
-        List<Profile> profiles = new ArrayList<>();
-        profiles.add(Profile.builder()
-                .id(1L)
-                .userId(1L)
-                .nickname("프로필1")
-                .build());
-
-        profiles.add(Profile.builder()
-                .id(2L)
-                .userId(1L)
-                .nickname("프로필2")
-                .build());
-
-        Mockito.when(profileRepository.findByUserId(1L)).thenReturn(profiles);
-
         회원가입_요청(JoinRequest.of("gitchan", "webos"));
         final String sessionId = 로그인_요청하고_세션_아이디_반환(LoginRequest.of("gitchan", "webos"));
+
+        final ProfileRequest profileRequest1 = ProfileRequest.builder()
+                .nickname("gitchan1")
+                .profileUri("https://3rkewj4g32rk41r5t45r")
+                .grade(Grade.ADULT)
+                .profilePassword("0123")
+                .build();
+
+        final ProfileRequest profileRequest2 = ProfileRequest.builder()
+                .nickname("gitchan2")
+                .profileUri("https://3rkewj4g32rk41r5t45r")
+                .grade(Grade.CHILD)
+                .profilePassword("0123")
+                .build();
+
+        final ProfileRequest profileRequest3 = ProfileRequest.builder()
+                .nickname("gitchan3")
+                .profileUri("https://3rkewj4g32rk41r5t45r")
+                .grade(Grade.ADULT)
+                .profilePassword("2345")
+                .build();
+
+        프로필_생성_요청(sessionId, profileRequest1);
+        프로필_생성_요청(sessionId, profileRequest2);
+        프로필_생성_요청(sessionId, profileRequest3);
 
         // when
         final ExtractableResponse<Response> response = RestAssured.given()
@@ -110,10 +107,11 @@ class ProfileAcceptanceTest extends AcceptanceTest {
 
         // then
         assertThat(response.statusCode()).isEqualTo(HttpStatus.OK.value());
+        assertThat(response.jsonPath().getList("profiles")).hasSize(3);
     }
 
     @Test
-    void server_profile_historyList() {
+    void 프로필별_시청_기록을_조회할_수_있다() {
         // given
         List<ProfileContents> profileContentsList = new ArrayList<>();
         ProfileContents profileContent1 = ProfileContents.builder().profileId(1L).contentsId(1L).state(ProfileContents.State.WATCHING).build();
