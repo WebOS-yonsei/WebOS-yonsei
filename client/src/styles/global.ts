190bdf106feb4d112784b164b30caa0143fad0b8
jinho222 <jinho020@yonsei.ac.kr>
2024-05-22 11:52:28 +0900

    [client] refactor: remove sandstone styling

--- client/src/styles/global.ts
+++ client/src/styles/global.ts
@@ -5,19 +5,8 @@ export const globalStyles = css`
     box-sizing: border-box;
   }
 
-  .enact-fit {
-    min-height: 100vh;
-    bottom: auto;
-
-    /* theme decorator */
-    & > & {
-      position: static;
-      padding: 0;
-    }
-  }
-
   // FIXME: 일단 24px로 고정해두긴하는데... 이건 나중에 수정해야할듯
   html {
-    font-size: 24px !important;
+    font-size: 24px;
   }
 `;
