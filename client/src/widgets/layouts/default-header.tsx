535a71110b228e92300f1030b6f9b33058f2262e
Jinho Choi <66049045+jinoov@users.noreply.github.com>
2024-05-28 17:24:00 +0900

    [client] feat: 프로필 나가기 API 연동 (#119)

--- client/src/widgets/layouts/default-header.tsx
+++ client/src/widgets/layouts/default-header.tsx
@@ -1,8 +1,41 @@
 import { SettingsIcon } from '@chakra-ui/icons';
 import { Box, Avatar, Button, Menu, MenuButton, MenuList, MenuItem, IconButton, HStack } from '@chakra-ui/react';
+import { useNavigate } from '@tanstack/react-router';
+import { MouseEventHandler } from 'react';
+import { client } from '~/features/@api';
+import { useUser } from '~/features/user';
+import { assert } from '~/utils';
 import { Link, VectorLogo } from '~/widgets';
 
 export function DefaultHeader() {
+  const naviagate = useNavigate();
+  const sessionId = useUser((state) => state.sessionId);
+
+  assert(sessionId, 'sessionId가 비어있음');
+
+  const onProfileLinkClick: MouseEventHandler<HTMLAnchorElement> = async (e) => {
+    e.preventDefault();
+
+    const { error } = await client.POST('/profiles/exit', {
+      params: {
+        query: {
+          user: {
+            sessionId,
+          },
+        },
+      },
+    });
+
+    const link = e.currentTarget.getAttribute('href');
+    assert(link, 'href가 비어있음');
+
+    if (!error) {
+      naviagate({
+        to: link,
+      });
+    }
+  };
+
   return (
     <Box py={2} px={4} as="nav" pos="fixed" top={0} left={0} width="100%" bgColor="#0c0c0f" alignItems="center" justifyContent="space-between" display="flex" zIndex={100}>
       <Link to="/video/list">
@@ -17,7 +50,7 @@ export function DefaultHeader() {
             <Avatar size="sm" src="https://images.unsplash.com/photo-1493666438817-866a91353ca9?ixlib=rb-0.3.5&q=80&fm=jpg&crop=faces&fit=crop&h=200&w=200&s=b616b2c5b373a80ffc9636ba24f7a4a9" />
           </MenuButton>
           <MenuList>
-            <Link to="/profile">
+            <Link to="/profile" onClick={onProfileLinkClick}>
               <MenuItem color="black" fontSize="20px">
                 프로필변경
               </MenuItem>
