dfbd5fb3e054504645f93e8eb0ea150f078e762a
Jinho Choi <66049045+jinoov@users.noreply.github.com>
2024-05-28 17:15:17 +0900

    [client] feat: 비디오 재생시간을 서버에 전송 (#116)
    * [client] feat: video 멈추는 지점을 체크
    
    * [client] feat: 비디오 재생시간 서버에 전송

--- client/src/features/video/video-player.tsx
+++ client/src/features/video/video-player.tsx
@@ -1,9 +1,36 @@
 import Hls from 'hls.js';
 import { useEffect, useRef } from 'react';
 import { assert } from '~/utils';
+import { useUser } from '../user';
+import { client } from '../@api';
+import { useCallbackRef } from '~/hooks';
 
-export function VideoPlayer({ src }: { src: string }) {
+export function VideoPlayer({ src, videoId }: { src: string; videoId: number }) {
   const videoRef = useRef<HTMLVideoElement | null>(null);
+  const profileId = useUser((state) => state.profileId);
+  const sessionId = useUser((state) => state.sessionId);
+
+  assert(profileId, 'profileId가 비어있음');
+  assert(sessionId, 'sessionId가 비어있음');
+
+  const recordPlayingTime = useCallbackRef((time: number) => {
+    client.POST('/videos/{videoId}/time/{profileId}', {
+      params: {
+        path: {
+          videoId,
+          profileId,
+        },
+        query: {
+          user: {
+            sessionId,
+          },
+        },
+      },
+      body: {
+        time,
+      },
+    });
+  });
 
   useEffect(() => {
     const video = videoRef.current;
@@ -15,10 +42,8 @@ export function VideoPlayer({ src }: { src: string }) {
       hls = new Hls();
       hls.loadSource(src);
       hls.attachMedia(video);
-      hls.on(Hls.Events.FRAG_LOADED, () => {
-        // TODO: send to server
-        // eslint-disable-next-line no-console
-        console.log('>>> current time: ', video.currentTime);
+      hls.on(Hls.Events.FRAG_PARSED, () => {
+        recordPlayingTime(video.currentTime);
       });
       hls.on(Hls.Events.MANIFEST_PARSED, () => {
         navigator.mediaDevices.getUserMedia({ video: true }).then(() => {
@@ -29,8 +54,15 @@ export function VideoPlayer({ src }: { src: string }) {
       video.src = src;
     }
 
-    return () => hls?.destroy();
-  }, [src]);
+    const onVideoEnded = () => recordPlayingTime(video.duration);
+
+    video.addEventListener('ended', onVideoEnded);
+
+    return () => {
+      hls?.destroy();
+      video.removeEventListener('ended', onVideoEnded);
+    };
+  }, [src, recordPlayingTime]);
 
   // eslint-disable-next-line jsx-a11y/media-has-caption
   return <video ref={videoRef} controls />;
--- client/src/features/video/video-playing-page.tsx
+++ client/src/features/video/video-playing-page.tsx
@@ -1,14 +1,14 @@
 import { Heading, VStack } from '@chakra-ui/react';
 import { VideoPlayer } from './video-player';
 
-export function VideoPlayingPage({ videoId }: { videoId: string }) {
+export function VideoPlayingPage({ videoId }: { videoId: number }) {
   const MOCK_URL = 'https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8';
 
   return (
     <VStack align="stretch" spacing={20}>
       <VStack align="stretch" spacing={5}>
         <Heading fontSize="2xl">{videoId}</Heading>
-        <VideoPlayer src={MOCK_URL} />
+        <VideoPlayer src={MOCK_URL} videoId={videoId} />
       </VStack>
     </VStack>
   );
