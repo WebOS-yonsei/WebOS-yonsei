be01db3a8f948fb9a04c27294c6e125e3ee859fb
jinoov <jinho020@yonsei.ac.kr>
2024-05-30 23:24:46 +0900

    [client] feat: 비디오 상세정보 페이지 데이터 바인딩

--- client/src/features/video/video-detail-page.tsx
+++ client/src/features/video/video-detail-page.tsx
@@ -1,14 +1,15 @@
 import { Heading, VStack, Image, Button, HStack, Text } from '@chakra-ui/react';
 import { Link } from '~/widgets';
+import { components } from '../@api';
 
-export function VideoDetailPage({ videoId }: { videoId: string }) {
+export function VideoDetailPage({ videoId, videoInfo }: { videoId: number; videoInfo: components['schemas']['Contents'] }) {
   return (
     <VStack align="stretch" spacing={20}>
       <VStack align="stretch" spacing={5}>
         <Heading fontSize="2xl">영상 상세</Heading>
         <HStack spacing={8} align="stretch">
           <VStack align="stretch">
-            <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" aspectRatio="3 / 2" />
+            <Image src={videoInfo.thumbnailURI} alt={videoInfo.title} aspectRatio="3 / 2" />
             <Link to="/video/$videoId/playing" params={{ videoId }}>
               <Button w="100%" colorScheme="red">
                 재생하기
@@ -16,8 +17,11 @@ export function VideoDetailPage({ videoId }: { videoId: string }) {
             </Link>
           </VStack>
           <VStack align="stretch" w="100%">
-            <Heading fontSize="2xl">동영상 제목</Heading>
-            <Text>동영상 설명</Text>
+            <Heading fontSize="2xl">{videoInfo.title}</Heading>
+            <Text>{videoInfo.description}</Text>
+            <Text>{videoInfo.genre}</Text>
+            <Text>{videoInfo.duration}초</Text>
+            <Text>{videoInfo.grade === 'ADULT' ? '성인' : '전체연령가'}</Text>
           </VStack>
         </HStack>
       </VStack>
