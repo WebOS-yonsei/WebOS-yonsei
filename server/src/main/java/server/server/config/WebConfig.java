a0148b6ed8b490d5efa07088d2067a2ea5bda86f
jinho222 <jinho020@yonsei.ac.kr>
2024-06-03 11:29:07 +0900

    [server] feat: cors 풀어놓기

--- server/src/main/java/server/server/config/WebConfig.java
+++ server/src/main/java/server/server/config/WebConfig.java
@@ -3,6 +3,7 @@ package server.server.config;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import server.server.config.resolver.UsersAuthArgumentResolver;
 
@@ -17,5 +18,15 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(usersAuthArgumentResolver);
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        String ALLOWED_METHOD_NAMES = "GET,HEAD,POST,PUT,DELETE,TRACE,OPTIONS,PATCH";
+
+        registry.addMapping("/**")
+                .allowedOrigins("*")
+                .allowedMethods(ALLOWED_METHOD_NAMES.split(","))
+                .maxAge(3600);
     }
 }
