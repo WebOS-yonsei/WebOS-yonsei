009177f087f1b57b84860228e68c8ccb0b291837
jinho222 <jinho020@yonsei.ac.kr>
2024-06-04 16:26:42 +0900

    [client] feat: add sessionId in header

--- client/src/features/@api/client.ts
+++ client/src/features/@api/client.ts
@@ -1,4 +1,17 @@
-import createClient from 'openapi-fetch';
+import createClient, { Middleware } from 'openapi-fetch';
 import type { paths } from './scheme';
+import { useUser } from '../user';
+
+const authMiddleware: Middleware = {
+  async onRequest(req) {
+    const { sessionId } = useUser.getState();
+    if (sessionId) {
+      req.headers.set('Authorization', `${sessionId}`);
+    }
+    return req;
+  },
+};
 
 export const client = createClient<paths>({ baseUrl: process.env.REACT_APP_SERVER_BASE_URL });
+
+client.use(authMiddleware);
