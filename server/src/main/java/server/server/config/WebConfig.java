270edb54de882c6d615cd713637f1061a7f0c2bd
Kiara <80676180+2020147542@users.noreply.github.com>
2024-06-05 09:54:47 +0900

    [server] fix: 런타임에 static 파일 읽어오도록 해결 (#158)

--- server/src/main/java/server/server/config/WebConfig.java
+++ server/src/main/java/server/server/config/WebConfig.java
@@ -4,6 +4,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import server.server.config.resolver.UsersAuthArgumentResolver;
 
@@ -30,4 +31,11 @@ public class WebConfig implements WebMvcConfigurer {
                 .allowedHeaders("Content-Type", "Authorization")
                 .maxAge(3600);
     }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/upload/**")
+                .addResourceLocations("file:src/main/resources/static/upload/");
+    }
+
 }
