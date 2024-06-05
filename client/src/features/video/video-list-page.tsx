35054cd7a0dc9ef03b267e37584c8d25a6f4676e
Jinho Choi <66049045+jinoov@users.noreply.github.com>
2024-06-05 13:44:11 +0900

    [client] fix: search params와 grid를 이용해서 비디오 감상 페이지 수정 (#171)

--- client/src/features/video/video-list-page.tsx
+++ client/src/features/video/video-list-page.tsx
@@ -1,50 +1,39 @@
-import { Heading, VStack, Wrap, WrapItem, Image, Text } from '@chakra-ui/react';
-import { Link, Slider } from '~/widgets';
+import { Heading, VStack, Tab, TabList, TabPanel, TabPanels, Tabs } from '@chakra-ui/react';
+import { useNavigate } from '@tanstack/react-router';
 import { components } from '../@api';
+import { VideoRecommendShell } from './video-recommend-shell';
+import { VideoWatchingShell } from './video-watching-shell';
 
-export function VideoListPage({
-  videoList,
-  historyList,
-  user,
-}: {
-  videoList: components['schemas']['Contents'][];
-  historyList: components['schemas']['Contents'][];
-  user: components['schemas']['CurrentUserResponse'];
-}) {
+export function VideoListPage({ videoList, user, index }: { videoList: components['schemas']['Contents'][]; user: components['schemas']['CurrentUserResponse']; index: 0 | 1 }) {
+  const navigate = useNavigate();
+
   return (
-    <VStack align="stretch" spacing={20}>
+    <VStack align="stretch" spacing={10}>
       <VStack align="stretch" spacing={5}>
-        <Heading fontSize="2xl">{user.nickname}님의 취향 저격 베스트 콘텐츠</Heading>
-        <Slider speed={500} slidesToShow={3} slidesToScroll={1} infinite={false}>
-          {videoList.map((video) => (
-            <Link to="/video/$videoId" params={{ videoId: video.id }} flexShrink={0} key={video.id}>
-              <VStack align="stretch">
-                <Image src={video.thumbnailURI} alt={video.title} aspectRatio="3 / 2" border="1px solid #eee" />
-                <Heading fontSize="md">{video.title}</Heading>
-              </VStack>
-            </Link>
+        <Heading fontSize="2xl">콘텐츠 감상</Heading>
+      </VStack>
+      <Tabs
+        variant="soft-rounded"
+        colorScheme="red"
+        index={index}
+        onChange={(i) =>
+          navigate({
+            search: () => ({ index: i }),
+          })
+        }
+      >
+        <TabList>
+          {['추천', '시청중'].map((name, i) => (
+            <Tab key={i}>{name}</Tab>
           ))}
-        </Slider>
-      </VStack>
-      <VStack align="stretch" spacing={5}>
-        <Heading fontSize="2xl">{user.nickname}님이 시청 중인 콘텐츠</Heading>
-        {historyList.length === 0 ? (
-          <Text fontSize="md">시청 중인 콘텐츠가 없습니다.</Text>
-        ) : (
-          <Wrap spacing="30px">
-            {historyList.map((video) => (
-              <WrapItem key={video.id}>
-                <Link to="/video/$videoId" params={{ videoId: video.id }}>
-                  <VStack align="stretch">
-                    <Image src={video.thumbnailURI} alt={video.title} aspectRatio="3 / 2" width={300} border="1px solid #eee" />
-                    <Heading fontSize="md">{video.title}</Heading>
-                  </VStack>
-                </Link>
-              </WrapItem>
-            ))}
-          </Wrap>
-        )}
-      </VStack>
+        </TabList>
+        <TabPanels>
+          {[<VideoRecommendShell videoList={videoList} user={user} />, <VideoWatchingShell videoList={videoList} user={user} />].map((component, i) => (
+            <TabPanel key={i}>{component}</TabPanel>
+          ))}
+          <TabPanel />
+        </TabPanels>
+      </Tabs>
     </VStack>
   );
 }
