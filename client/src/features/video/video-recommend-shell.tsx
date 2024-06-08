30531e59838393092708f16f12774bb097811694
jinoov <jinho020@yonsei.ac.kr>
2024-06-09 02:09:41 +0900

    [client] feat: grid를 flex wrap으로 대체

--- client/src/features/video/video-recommend-shell.tsx
+++ client/src/features/video/video-recommend-shell.tsx
@@ -1,4 +1,4 @@
-import { Heading, Text, Image, Grid, GridItem } from '@chakra-ui/react';
+import { Heading, Text, Image, Wrap, WrapItem } from '@chakra-ui/react';
 import { Link } from '@tanstack/react-router';
 import { components } from '../@api';
 import { VStack } from '~/widgets';
@@ -6,22 +6,24 @@ import { VStack } from '~/widgets';
 export function VideoRecommendShell({ user, videoList }: { videoList: components['schemas']['Contents'][]; user: components['schemas']['CurrentUserResponse'] }) {
   return (
     <VStack align="stretch" spacing={5}>
-      <Heading fontSize="xl">{user.nickname}님의 취향 저격 베스트 콘텐츠</Heading>
+      <Heading fontSize="xl" px={2}>
+        {user.nickname}님의 취향 저격 베스트 콘텐츠
+      </Heading>
       {videoList.length === 0 ? (
         <Text fontSize="md">시청 중인 콘텐츠가 없습니다.</Text>
       ) : (
-        <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+        <Wrap spacing={0}>
           {videoList.map((video) => (
-            <GridItem key={video.id}>
+            <WrapItem key={video.id} w="25%" p={2}>
               <Link to="/video/$videoId" params={{ videoId: String(video.id) }}>
                 <VStack align="stretch">
-                  <Image src={video.thumbnailURI} alt={video.title} aspectRatio="3 / 2" border="1px solid #eee" />
+                  <Image src={video.thumbnailURI} alt={video.title} aspectRatio="3 / 2" border="1px solid #eee" maxW="100%" />
                   <Heading fontSize="md">{video.title}</Heading>
                 </VStack>
               </Link>
-            </GridItem>
+            </WrapItem>
           ))}
-        </Grid>
+        </Wrap>
       )}
     </VStack>
   );
--- client/src/features/video/video-watching-shell.tsx
+++ client/src/features/video/video-watching-shell.tsx
@@ -1,4 +1,4 @@
-import { Heading, Image, Text, Grid, GridItem } from '@chakra-ui/react';
+import { Heading, Image, Text, Wrap, WrapItem } from '@chakra-ui/react';
 import { Link } from '@tanstack/react-router';
 import { components } from '../@api';
 import { VStack } from '~/widgets';
@@ -6,22 +6,24 @@ import { VStack } from '~/widgets';
 export function VideoWatchingShell({ user, videoList }: { videoList: components['schemas']['Contents'][]; user: components['schemas']['CurrentUserResponse'] }) {
   return (
     <VStack align="stretch" spacing={5}>
-      <Heading fontSize="xl">{user.nickname}님이 시청 중인 콘텐츠</Heading>
+      <Heading fontSize="xl" px={2}>
+        {user.nickname}님이 시청 중인 콘텐츠
+      </Heading>
       {videoList.length === 0 ? (
         <Text fontSize="md">시청 중인 콘텐츠가 없습니다.</Text>
       ) : (
-        <Grid templateColumns="repeat(4, 1fr)" gap={6}>
+        <Wrap spacing={0}>
           {videoList.map((video) => (
-            <GridItem key={video.id}>
+            <WrapItem key={video.id} w="25%" p={2}>
               <Link to="/video/$videoId" params={{ videoId: String(video.id) }}>
                 <VStack align="stretch">
-                  <Image src={video.thumbnailURI} alt={video.title} aspectRatio="3 / 2" border="1px solid #eee" />
+                  <Image src={video.thumbnailURI} alt={video.title} aspectRatio="3 / 2" border="1px solid #eee" maxW="100%" />
                   <Heading fontSize="md">{video.title}</Heading>
                 </VStack>
               </Link>
-            </GridItem>
+            </WrapItem>
           ))}
-        </Grid>
+        </Wrap>
       )}
     </VStack>
   );
