aed0054b013fe09115c8149ac9d15172a59c2c75
jinoov <jinho020@yonsei.ac.kr>
2024-06-09 02:10:19 +0900

    [client] feat: scroll 수치 늘리기

--- client/src/widgets/scroll-button-group.tsx
+++ client/src/widgets/scroll-button-group.tsx
@@ -5,8 +5,8 @@ import { VStack } from './vstack';
 export function ScrollButtonGroup() {
   return (
     <VStack pos="fixed" bottom={3} right={3}>
-      <IconButton icon={<ArrowUpIcon />} aria-label="up" onClick={() => window.scrollBy({ top: -64 })} rounded="full" />
-      <IconButton icon={<ArrowDownIcon />} aria-label="down" onClick={() => window.scrollBy({ top: 64 })} rounded="full" />
+      <IconButton icon={<ArrowUpIcon />} aria-label="up" onClick={() => window.scrollBy({ top: -200 })} rounded="full" />
+      <IconButton icon={<ArrowDownIcon />} aria-label="down" onClick={() => window.scrollBy({ top: 200 })} rounded="full" />
     </VStack>
   );
 }
